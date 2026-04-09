78cc1e535fe552bec08fd85fadb39c150bb045dc
Varshith2568 <varshithreddyr.2007@gmail.com>
2026-04-09 09:09:41 +0530

    UC7: Sort Bogies by Capacity using Comparator

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -21,6 +21,9 @@ public class TrainConsistManagementApp {
 
         // ===== UC6 =====
         mapBogieCapacity();
+
+        // ===== UC7 =====
+        sortBogiesByCapacity();
     }
 
     // ===== UC1 =====
@@ -176,6 +179,51 @@ public class TrainConsistManagementApp {
         }
 
         System.out.println("\nUC6 bogie-capacity mapping completed...\n");
+    }
+
+    // ===== UC7 =====
+    public static void sortBogiesByCapacity() {
+
+        System.out.println("======================================");
+        System.out.println("UC7 - Sort Bogies by Capacity (Comparator)");
+        System.out.println("======================================\n");
+
+        // Inner Bogie class
+        class Bogie {
+            String name;
+            int capacity;
+
+            Bogie(String name, int capacity) {
+                this.name = name;
+                this.capacity = capacity;
+            }
+        }
+
+        // Create list
+        List<Bogie> bogies = new ArrayList<>();
+
+        // Add bogies
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("General", 90));
+
+        // BEFORE SORTING
+        System.out.println("Before Sorting:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
+        // SORT USING COMPARATOR
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        // AFTER SORTING
+        System.out.println("\nAfter Sorting by Capacity:");
+        for (Bogie b : bogies) {
+            System.out.println(b.name + " -> " + b.capacity);
+        }
+
+        System.out.println("\nUC7 sorting completed...\n");
     }
 }
 
