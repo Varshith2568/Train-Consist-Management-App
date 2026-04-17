4970226bdc74c36464cf973be01e2427ba8d8d2c
Varshith2568 <varshithreddyr.2007@gmail.com>
2026-04-17 10:16:07 +0530

    UC16: Bubble Sort Implementation

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -20,6 +20,7 @@ public class TrainConsistManagementApp {
         performanceComparison();        // UC13
         handleInvalidCapacity();       // UC14
         safeCargoAssignment();        // UC15
+        bubbleSortCapacities();   // UC16
     }
 
     // ===== UC1 =====
@@ -402,17 +403,52 @@ public class TrainConsistManagementApp {
             }
         }
 
-        // ✅ Safe case
+        //  Safe case
         GoodsBogie g1 = new GoodsBogie("Cylindrical");
         g1.assignCargo("Petroleum");
 
         System.out.println();
 
-        // ❌ Unsafe case
+        //  Unsafe case
         GoodsBogie g2 = new GoodsBogie("Rectangular");
         g2.assignCargo("Petroleum");
 
         System.out.println("\nUC15 runtime handling completed...\n");
+    }
+
+    // ===== UC16 =====
+    public static void bubbleSortCapacities() {
+        System.out.println("=====================================");
+        System.out.println("UC16 - Manual Sorting using Bubble Sort");
+        System.out.println("=====================================\n");
+
+        int[] capacities = {72, 56, 24, 70, 60};
+
+        // Display original
+        System.out.println("Original Capacities:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < capacities.length - 1; i++) {
+            for (int j = 0; j < capacities.length - i - 1; j++) {
+
+                if (capacities[j] > capacities[j + 1]) {
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        // Display sorted
+        System.out.println("\n\nSorted Capacities (Ascending):");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        System.out.println("\n\nUC16 sorting completed...\n");
     }
 }
 
